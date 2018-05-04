0245336f13a53130a8e7e661e4dac09bb6caa7b6
Ryan Gabbard <gabbard@isi.edu>
2018-05-04 15:32:56 -0400

    Fix bug which caused validation to fail

--- src/main/java/edu/isi/gaia/ValidateAIF.kt
+++ src/main/java/edu/isi/gaia/ValidateAIF.kt
@@ -6,21 +6,20 @@ import com.google.common.collect.Iterators
 import com.google.common.io.Files
 import com.google.common.io.Resources
 import edu.isi.nlp.parameters.Parameters
-import edu.isi.nlp.parameters.serifstyle.SerifStyleParameterFileLoader
-import org.apache.jena.query.QueryExecutionFactory
-import org.apache.jena.query.QueryFactory
+import mu.KLogging
 import org.apache.jena.rdf.model.Model
 import org.apache.jena.rdf.model.ModelFactory
-import org.apache.jena.rdf.model.RDFNode
-import org.apache.jena.rdf.model.Resource
+import org.apache.jena.sparql.function.library.leviathan.log
 import org.apache.jena.util.FileUtils
-import org.apache.jena.vocabulary.RDF
 import org.topbraid.shacl.validation.ValidationUtil
 import java.io.File
 import java.io.Reader
 
+
+
 fun loadModel(reader: Reader) : Model {
     val ret = ModelFactory.createOntologyModel()
+
     ret.read(reader, "urn:x-base", FileUtils.langTurtle)
     return ret
 }
@@ -34,95 +33,47 @@ val LACKS_TYPES_QUERY = """
     }
     """
 
-fun requireSingleContentGraph(model: Model) : Resource {
-    val numContentGraphs = Iterators.size(
-            model.listSubjectsWithProperty(RDF.type, AidaAnnotationOntology.KNOWLEDGE_GRAPH))
-    if (numContentGraphs == 0) {
-        print("Input lacks resource with type aida:KnowledgeGraph")
-        System.exit(1)
-    } else if (numContentGraphs > 1) {
-        print("Input has multiple aida:KnowledgeGraphs")
-        System.exit(1)
-    }
-    val rootGraph = Iterators.getOnlyElement(
-            model.listSubjectsWithProperty(RDF.type, AidaAnnotationOntology.KNOWLEDGE_GRAPH))
+object ValidateAIF {
 
-    if (!rootGraph.isURIResource) {
-        print("Root graph must have a URI")
-        System.exit(1)
-    }
+    val log = KLogging()
 
-    return rootGraph
-}
-
-val CONNECTED_COMPONENTS_QUERY = """
-prefix dummy: <urn:dummy:>
-
-construct { ?s ?p ?o }
-where {<%s> (dummy:|!dummy:)* ?s . ?s ?p ?o .}
-"""
-
-fun requireConnectedToContentGraph(model: Model, rootGraphURI: String) {
-    val query = CONNECTED_COMPONENTS_QUERY.format(rootGraphURI)
-    val nodes = mutableSetOf<RDFNode>()
-    QueryExecutionFactory.create(QueryFactory.create(query), model).use {
-        for (statement in it.execConstruct().listStatements()) {
-            nodes.add(statement.subject)
-            nodes.add(statement.`object`)
+    @JvmStatic
+    fun main(args: Array<String>) {
+        if (args.size != 1) {
+            print("Usage: validateGraph paramFile")
+            System.exit(1)
         }
-    }
-    var foundDisconnectedSubject = false
-    for (subject in model.listSubjects()) {
-        if (!nodes.contains(subject)) {
-            val name = subject.uri ?: "blank node"
-            // TODO: hack
-            if (!name.startsWith("http://www.w3.org")) {
-                print("Subject $name is not connected to the root graph $rootGraphURI\n")
-                foundDisconnectedSubject = true
-            }
+
+        val params = Parameters.loadSerifStyle(File(args[0]))
+
+        // this is an RDF model which uses SHACL to encode constraints on the AIF
+        val shapeModel = Resources.asCharSource(
+                Resources.getResource("edu/isi/gaia/aida_ontology.shacl"), Charsets.UTF_8)
+                .openBufferedStream().use {
+            loadModel(it)
         }
-    }
-    if (foundDisconnectedSubject) {
-        System.exit(1)
-    }
-}
 
-fun main(args: Array<String>) {
-    if (args.size != 1) {
-        print("Usage: validateGraph paramFile")
-        System.exit(1)
-    }
+        val fileList = params.getOptionalExistingFile("kbsToValidate")
+        val filesToValidate = if (fileList.isPresent) {
+            edu.isi.nlp.files.FileUtils.loadFileList(fileList.get())
+        } else {
+            ImmutableList.of(params.getExistingFile("kbToValidate"))
+        }
 
-    val params = Parameters.loadSerifStyle(File(args[0]))
+        for (fileToValidate in filesToValidate) {
+            log.logger.info { "Validating $fileToValidate" }
+            val dataToBeValidated = Files.asCharSource(fileToValidate, Charsets.UTF_8)
+                    .openBufferedStream().use { loadModel(it) }
+            validateKB(shapeModel, dataToBeValidated)
+        }
 
-    // this is an RDF model which uses SHACL to encode constraints on the AIF
-    val shapeModel = Resources.asCharSource(
-            Resources.getResource("edu/isi/gaia/aida_ontology.shacl"), Charsets.UTF_8)
-            .openBufferedStream().use {
-        loadModel(it)
+        // TODO: return non-zero exit code on failure
     }
 
-    val fileList = params.getOptionalExistingFile("kbsToValidate")
-    val filesToValidate = if (fileList.isPresent) {
-        edu.isi.nlp.files.FileUtils.loadFileList(fileList.get())
-    } else {
-        ImmutableList.of(params.getExistingFile("kbToValidate"))
+    private fun validateKB(shapeModel: Model, dataToBeValidated: Model) {
+        // do SHACL validation
+        val report = ValidationUtil.validateModel(dataToBeValidated, shapeModel, true)
+        report.model.write(System.out, FileUtils.langTurtle)
+
     }
-
-    for (fileToValidate in filesToValidate) {
-        val dataToBeValidated = Files.asCharSource(fileToValidate, Charsets.UTF_8)
-                .openBufferedStream().use { loadModel(it) }
-        validateKB(shapeModel, dataToBeValidated)
-    }
-
-    // TODO: return non-zero exit code on failure
-}
-
-private fun validateKB(shapeModel: Model, dataToBeValidated: Model) {
-    // do SHACL validation
-    val report = ValidationUtil.validateModel(shapeModel, dataToBeValidated, true)
-    report.model.write(System.out, FileUtils.langTurtle)
-
-    val rootGraph = requireSingleContentGraph(dataToBeValidated)
-    requireConnectedToContentGraph(dataToBeValidated, rootGraph.uri)
 }
