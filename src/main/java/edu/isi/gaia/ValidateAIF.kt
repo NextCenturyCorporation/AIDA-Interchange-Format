95b50fac5def881a3ee91e89a0ed913de08fa1f2
Ryan Gabbard <gabbard@isi.edu>
2018-05-04 12:37:22 -0400

    Support validating multiple KBs in one run

--- src/main/java/edu/isi/gaia/ValidateAIF.kt
+++ src/main/java/edu/isi/gaia/ValidateAIF.kt
@@ -1,29 +1,23 @@
 package edu.isi.gaia
 
 import com.google.common.base.Charsets
+import com.google.common.collect.ImmutableList
 import com.google.common.collect.Iterators
 import com.google.common.io.Files
 import com.google.common.io.Resources
-import org.apache.commons.lang3.CharSet
-import org.apache.jena.enhanced.BuiltinPersonalities.model
+import edu.isi.nlp.parameters.Parameters
+import edu.isi.nlp.parameters.serifstyle.SerifStyleParameterFileLoader
 import org.apache.jena.query.QueryExecutionFactory
 import org.apache.jena.query.QueryFactory
 import org.apache.jena.rdf.model.Model
 import org.apache.jena.rdf.model.ModelFactory
 import org.apache.jena.rdf.model.RDFNode
 import org.apache.jena.rdf.model.Resource
-import org.apache.jena.sparql.vocabulary.EARL.failed
-import org.apache.jena.sparql.vocabulary.VocabTestQuery.query
 import org.apache.jena.util.FileUtils
 import org.apache.jena.vocabulary.RDF
 import org.topbraid.shacl.validation.ValidationUtil
-import org.topbraid.shacl.vocabulary.SH.nodes
-import org.topbraid.shacl.vocabulary.SH.not
 import java.io.File
-import java.io.InputStream
 import java.io.Reader
-import java.nio.file.Paths
-import kotlin.coroutines.experimental.EmptyCoroutineContext.plus
 
 fun loadModel(reader: Reader) : Model {
     val ret = ModelFactory.createOntologyModel()
@@ -95,24 +89,40 @@ fun requireConnectedToContentGraph(model: Model, rootGraphURI: String) {
 
 fun main(args: Array<String>) {
     if (args.size != 1) {
-        print("Usage: validateGraph graph-rdf-file")
+        print("Usage: validateGraph paramFile")
         System.exit(1)
     }
+
+    val params = Parameters.loadSerifStyle(File(args[0]))
+
+    // this is an RDF model which uses SHACL to encode constraints on the AIF
     val shapeModel = Resources.asCharSource(
             Resources.getResource("edu/isi/gaia/aida_ontology.shacl"), Charsets.UTF_8)
             .openBufferedStream().use {
         loadModel(it)
     }
 
-    val dataModel = Files.asCharSource(File(args[0]), Charsets.UTF_8).openBufferedStream().use {
-        loadModel(it)
+    val fileList = params.getOptionalExistingFile("kbsToValidate")
+    val filesToValidate = if (fileList.isPresent) {
+        edu.isi.nlp.files.FileUtils.loadFileList(fileList.get())
+    } else {
+        ImmutableList.of(params.getExistingFile("kbToValidate"))
     }
 
+    for (fileToValidate in filesToValidate) {
+        val dataToBeValidated = Files.asCharSource(fileToValidate, Charsets.UTF_8)
+                .openBufferedStream().use { loadModel(it) }
+        validateKB(shapeModel, dataToBeValidated)
+    }
+
+    // TODO: return non-zero exit code on failure
+}
+
+private fun validateKB(shapeModel: Model, dataToBeValidated: Model) {
     // do SHACL validation
-    val report = ValidationUtil.validateModel(shapeModel, dataModel, true)
+    val report = ValidationUtil.validateModel(shapeModel, dataToBeValidated, true)
     report.model.write(System.out, FileUtils.langTurtle)
 
-    val rootGraph = requireSingleContentGraph(dataModel)
-    requireConnectedToContentGraph(dataModel, rootGraph.uri)
-
+    val rootGraph = requireSingleContentGraph(dataToBeValidated)
+    requireConnectedToContentGraph(dataToBeValidated, rootGraph.uri)
 }
