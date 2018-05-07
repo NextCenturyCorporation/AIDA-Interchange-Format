29a3d0dc76ca74fe92cb840923052acbbb264ac6
Ryan Gabbard <gabbard@isi.edu>
2018-05-07 12:50:25 -0400

    Add comments to AIFUtils (#13)

--- src/main/java/edu/isi/gaia/AIFUtils.kt
+++ src/main/java/edu/isi/gaia/AIFUtils.kt
@@ -1,2 +1,136 @@
 package edu.isi.gaia
 
+import org.apache.jena.rdf.model.Model
+import org.apache.jena.rdf.model.Resource
+import org.apache.jena.vocabulary.RDF
+
+/**
+ * A convenient interface for creating simple AIF graphs.
+ *
+ * More complicated graphs will require direct manipulation of the RDF.
+ *
+ * @author Ryan Gabbard (USC ISI)
+ */
+object AIFUtils {
+    /**
+     * Create a resource representing the system which produced some data.
+     *
+     * Such a resource should be attached to all entities, events, event arguments, relations,
+     * sentiment assertions, confidences, justifications, etc. produced by a system. You should
+     * only create the system resource once; reuse the returned objects for all calls
+     * to [markSystem].
+     *
+     * @return The created system resource.
+     */
+    @JvmStatic
+    fun makeSystemWithURI(model: Model, systemURI: String): Resource {
+        val system = model.createResource(systemURI)!!
+        system.addProperty(RDF.type, AidaAnnotationOntology.SYSTEM_CLASS)
+        return system
+    }
+
+    /**
+     * Mark a resource as coming from the specified [system].
+     */
+    @JvmStatic
+    fun markSystem(toMarkOn: Resource, system: Resource) {
+        toMarkOn.addProperty(AidaAnnotationOntology.SYSTEM_PROPERTY, system)
+    }
+
+
+    /**
+     * Create an entity.
+     *
+     * @param [entityUri] can be any unique string.
+     * @param [system] The system object for the system which created this entity.
+     */
+    @JvmStatic
+    fun makeEntity(model: Model, entityUri: String, system: Resource): Resource {
+        val entity = model.createResource(entityUri)!!
+        entity.addProperty(RDF.type, AidaAnnotationOntology.ENTITY_CLASS)
+        entity.addProperty(AidaAnnotationOntology.SYSTEM_PROPERTY, system)
+        return entity
+    }
+
+    /**
+     * Mark an entity or event as having a specified type.
+     *
+     * This is marked with a separate assertion so that uncertainty about type can be expressed.
+     * In such a case, bundle together the type assertion resources returned by this method with
+     * [markAsMutuallyExclusive].
+     *
+     * @param [type] The type of the entity or event being asserted
+     * @param [system] The system object for the system which created this entity.
+     */
+    @JvmStatic
+    fun markType(model: Model, typeAssertionUri: String, entityOrEvent: Resource,
+                 type: Resource, system: Resource): Resource {
+        val typeAssertion = model.createResource(typeAssertionUri)!!
+        typeAssertion.addProperty(RDF.type, RDF.Statement)
+        typeAssertion.addProperty(RDF.subject, entityOrEvent)
+        typeAssertion.addProperty(RDF.predicate, RDF.type)
+        typeAssertion.addProperty(RDF.`object`, type)
+        typeAssertion.addProperty(AidaAnnotationOntology.SYSTEM_PROPERTY, system)
+        return typeAssertion
+    }
+
+    /**
+     * Mark something as being justified by a particular snippet of text.
+     *
+     * @return The text justification resource created.
+     */
+    @JvmStatic
+    fun markTextJustification(model: Model, toMarkOn: Resource, docId: String,
+                              startOffset: Int, endOffsetInclusive: Int,
+                              system: Resource): Resource {
+        return markTextJustification(model, setOf(toMarkOn), docId, startOffset,
+                endOffsetInclusive, system)
+    }
+
+    /**
+     * Mark multiple things as being justified by a particular snippet of text.
+     *
+     * @return The text justification resource created.
+     */
+    @JvmStatic
+    fun markTextJustification(model: Model, toMarkOn: Collection<Resource>, docId: String,
+                              startOffset: Int, endOffsetInclusive: Int,
+                              system: Resource): Resource {
+        // the justification provides the evidence for our claim about the entity's type
+        val justification = model.createResource()
+        // there can also be video justifications, audio justifications, etc.
+        justification.addProperty(RDF.type, AidaAnnotationOntology.TEXT_JUSTIFICATION_CLASS)
+        // the document ID for the justifying source document
+        justification.addProperty(AidaAnnotationOntology.SOURCE, model.createTypedLiteral(docId))
+        justification.addProperty(AidaAnnotationOntology.START_OFFSET,
+                model.createTypedLiteral(startOffset))
+        justification.addProperty(AidaAnnotationOntology.END_OFFSET_INCLUSIVE,
+                model.createTypedLiteral(endOffsetInclusive))
+        justification.addProperty(AidaAnnotationOntology.SYSTEM_PROPERTY, system)
+
+        toMarkOn.forEach({ it.addProperty(AidaAnnotationOntology.JUSTIFIED_BY, justification) })
+        return justification
+    }
+
+    /**
+     * Mark a confidence value on a resource.
+     */
+    @JvmStatic
+    fun markConfidence(model: Model, toMarkOn: Resource, confidence: Double, system: Resource) {
+        val confidenceBlankNode = model.createResource()
+        confidenceBlankNode.addProperty(RDF.type, AidaAnnotationOntology.CONFIDENCE_CLASS)
+        confidenceBlankNode.addProperty(AidaAnnotationOntology.CONFIDENCE_VALUE,
+                model.createTypedLiteral(confidence))
+        confidenceBlankNode.addProperty(AidaAnnotationOntology.SYSTEM_PROPERTY, system)
+        toMarkOn.addProperty(AidaAnnotationOntology.CONFIDENCE, confidenceBlankNode)
+    }
+
+    /**
+     * Mark the given resources as mutually exclusive.
+     */
+    @JvmStatic
+    fun markAsMutuallyExclusive(model: Model, toMark: Collection<Resource>,
+                                system: Resource): Resource {
+        throw NotImplementedError("Not implemented yet")
+    }
+}
--- src/test/java/edu/isi/gaia/ExamplesAndValidationTests.kt
+++ src/test/java/edu/isi/gaia/ExamplesAndValidationTests.kt
@@ -1,7 +1,10 @@
 package edu.isi.gaia
 
+import edu.isi.gaia.AIFUtils.makeEntity
+import edu.isi.gaia.AIFUtils.markConfidence
+import edu.isi.gaia.AIFUtils.markTextJustification
+import edu.isi.gaia.AIFUtils.markType
 import org.apache.jena.rdf.model.ModelFactory
-import org.apache.jena.vocabulary.RDF
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
@@ -20,46 +23,26 @@ internal class ExamplesAndValidationTests {
             val model = createModel()
 
             // every AIF needs an object for the system responsible for creating it
-            val system = model.createResource("http://www.test.edu/testSystem")
-            system.addProperty(RDF.type, AidaAnnotationOntology.SYSTEM_CLASS)
+            val system = AIFUtils.makeSystemWithURI(model, "http://www.test.edu/testSystem")
 
             // it doesn't matter what URI we give entities, events, etc. so long as they are
             // unique
-            val entity = model.createResource("http://www.test.org/entities/1")
-            entity.addProperty(RDF.type, AidaAnnotationOntology.ENTITY_CLASS)
+            val entity = makeEntity(model, "http://www.test.edu/entities/1", system)
 
             // in order to allow uncertainty about the type of an entity, we don't mark an
             // entity's type directly on the entity, but rather make a separate assertion for it
             // its URI doesn't matter either
-            val typeAssertion = model.createResource("http://www.test.org/assertions/1")
-            typeAssertion.addProperty(RDF.type, RDF.Statement)
-            typeAssertion.addProperty(RDF.subject, entity)
-            typeAssertion.addProperty(RDF.predicate, RDF.type)
-            typeAssertion.addProperty(RDF.`object`, AidaDomainOntology.PERSON)
+            val typeAssertion = markType(model, "http://www.test.org/assertions/1",
+                    entity, AidaDomainOntology.PERSON, system)
 
             // the justification provides the evidence for our claim about the entity's type
-            val justification = model.createResource()
-            // there can also be video justifications, audio justifications, etc.
-            justification.addProperty(RDF.type, AidaAnnotationOntology.TEXT_JUSTIFICATION_CLASS)
-            // the document ID for the justifying source document
-            justification.addProperty(AidaAnnotationOntology.SOURCE,
-                    model.createTypedLiteral("NYT_ENG_201181231"))
-            justification.addProperty(AidaAnnotationOntology.START_OFFSET,
-                    model.createTypedLiteral(14))
-            justification.addProperty(AidaAnnotationOntology.END_OFFSET_INCLUSIVE,
-                    model.createTypedLiteral(42))
             // we attach this justification to both the type assertion and the entity object
             // itself, since it provides evidence both for the entity's existence and its type.
-            entity.addProperty(AidaAnnotationOntology.JUSTIFIED_BY, justification)
-            typeAssertion.addProperty(AidaAnnotationOntology.JUSTIFIED_BY, justification)
+            val justification = markTextJustification(model, setOf(entity, typeAssertion),
+                    "NYT_ENG_201181231", 42, 14, system)
 
             // in TA1 -> TA2 communications, we attach confidences at the level of justifications
-            // TODO: should we enforce confidences from the same system are the same?
-            val confidenceBlankNode = model.createResource()
-            confidenceBlankNode.addProperty(RDF.type, AidaAnnotationOntology.CONFIDENCE_CLASS)
-            confidenceBlankNode.addProperty(AidaAnnotationOntology.CONFIDENCE_VALUE,
-                    model.createTypedLiteral(0.974))
-            justification.addProperty(AidaAnnotationOntology.CONFIDENCE, confidenceBlankNode)
+            markConfidence(model, justification, 0.973, system)
 
             assertTrue { validator.validateKB(model) }
         }
