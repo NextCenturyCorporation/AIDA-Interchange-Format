82b3a353944e7938be2ceff3a175a9a63f8c6cfb
jvanbramer <jakevb@gmail.com>
2019-02-04 11:31:59 -0500

    Added Ontologies and Ontology generated resources.

---
+++ src/main/java/com/ncc/aif/EntityRelations.java
@@ -0,0 +1,47 @@
+package com.ncc.aif;
+
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.ResourceFactory;
+
+public final class EntityRelations {
+    public static final Resource OrganizationAffiliation_InvestorShareholder = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#OrganizationAffiliation.InvestorShareholder");
+    public static final Resource GeneralAffiliation_APORA = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#GeneralAffiliation.APORA");
+    public static final Resource Make = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#Make");
+    public static final Resource Physical_LocatedNear = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#Physical.LocatedNear");
+    public static final Resource Physical_OrganizationLocationOrigin = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#Physical.OrganizationLocationOrigin");
+    public static final Resource Author = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#Author");
+    public static final Resource Material = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#Material");
+    public static final Resource Measurement_Count = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#Measurement.Count");
+    public static final Resource OrganizationAffiliation_EmploymentMembership = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#OrganizationAffiliation.EmploymentMembership");
+    public static final Resource PersonalSocial_Unspecified = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#PersonalSocial.Unspecified");
+    public static final Resource GeneralAffiliation_Sponsorship = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#GeneralAffiliation.Sponsorship");
+    public static final Resource Function = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#Function");
+    public static final Resource PersonalSocial_Family = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#PersonalSocial.Family");
+    public static final Resource OrganizationAffiliation_Ownership = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#OrganizationAffiliation.Ownership");
+    public static final Resource Color = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#Color");
+    public static final Resource Structure = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#Structure");
+    public static final Resource Physical_OrganizationHeadquarter = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#Physical.OrganizationHeadquarter");
+    public static final Resource Title = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#Title");
+    public static final Resource GeneralAffiliation_OPRA = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#GeneralAffiliation.OPRA");
+    public static final Resource Age = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#Age");
+    public static final Resource GeneralAffiliation_PersonAge = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#GeneralAffiliation.PersonAge");
+    public static final Resource LocRel = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#LocRel");
+    public static final Resource GeneralAffiliation_OrganizationWebsite = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#GeneralAffiliation.OrganizationWebsite");
+    public static final Resource OrganizationAffiliation_Founder = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#OrganizationAffiliation.Founder");
+    public static final Resource Employment = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#Employment");
+    public static final Resource PersonalSocial_Business = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#PersonalSocial.Business");
+    public static final Resource PersonalSocial_RoleTitle = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#PersonalSocial.RoleTitle");
+    public static final Resource Nationality = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#Nationality");
+    public static final Resource Content = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#Content");
+    public static final Resource Medium = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#Medium");
+    public static final Resource Status = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#Status");
+    public static final Resource OrganizationAffiliation_StudentAlum = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#OrganizationAffiliation.StudentAlum");
+    public static final Resource GeneralAffiliation_MORE = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#GeneralAffiliation.MORE");
+    public static final Resource Evaluation = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#Evaluation");
+    public static final Resource PartWhole_Subsidiary = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#PartWhole.Subsidiary");
+    public static final Resource OrganizationAffiliation_Leadership = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#OrganizationAffiliation.Leadership");
+    public static final Resource PartWhole_Membership = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#PartWhole.Membership");
+    public static final Resource Physical_Resident = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#Physical.Resident");
+    public static final Resource Side = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#Side");
+    public static final Resource Size = ResourceFactory.createResource("https://tac.nist.gov/tracks/SM-KBP/2018/ontologies/EntityRelations#Size");
+}
