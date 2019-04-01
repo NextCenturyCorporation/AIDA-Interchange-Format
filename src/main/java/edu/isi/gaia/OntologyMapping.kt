de2bd849212a2742f6dc96597ff991b7fdeb0ded
Ryan Gabbard <gabbard@isi.edu>
2019-04-01 12:46:31 -0400

    Update ColdStart converter for M18 (#150)
    * Remove obsolete ColdStart conversion code
    
    * Make ontology mapper for M18 evaluation
    
    * prefLabels restricted to debugging only
    
    * Update ColdStart converter for M18
    
    * Update ColdStart converter for M18
    
    - use event-like representation of relations
    - require white-listing untranslatable types

--- src/main/java/edu/isi/gaia/OntologyMapping.kt
+++ src/main/java/edu/isi/gaia/OntologyMapping.kt
@@ -3,34 +3,17 @@ package edu.isi.gaia
 import org.apache.jena.rdf.model.Resource
 
 /**
- * Memoizing class, used for implementing ontologies.
- */
-class OntoMemoize<in Arg, out Result>(val f: (Arg) -> Result) : (Arg) -> Result {
-    private val cache = mutableMapOf<Arg, Result>()
-    override fun invoke(arg: Arg) = cache.getOrPut(arg) { f(arg) }
-}
-
-/**
- * A domain ontology.
+ * Specifies how ColdStart type strings should be mapped to RDF IRIs.
  */
 interface OntologyMapping {
-    val NAMESPACE: String
-    fun entityShortNames(): Set<String>
+    fun prefixes(): Map<Resource, String>
     fun entityType(ontology_type: String): Resource?
-
     fun relationType(relationName: String): Resource?
     fun eventType(eventName: String): Resource?
     fun eventArgumentType(argName: String): Resource?
-    /**
-     * Given a relation, get its two argument types in the same order as in LDC annotation.
-     */
-    fun relationArgumentTypes(relation: Resource): Pair<Resource, Resource>
+    fun relationArgumentType(relationArgumentName: String): Resource?
 
-    /**
-     * Is an object of this type allowed to have a name property?
-     */
     fun typeAllowedToHaveAName(type: Resource): Boolean
-
     fun typeAllowedToHaveTextValue(type: Resource): Boolean
     fun typeAllowedToHaveNumericValue(type: Resource): Boolean
 }
