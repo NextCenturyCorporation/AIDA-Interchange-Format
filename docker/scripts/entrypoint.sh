4356f8a00450f6a949cb152c482ee48cd1ff2e9c
Patrick Sharkey <patrick.sharkey@nextcentury.com>
2019-04-24 16:40:55 -0400

    Updated entrypoint script to assist with debuggin aws batch

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -2,4 +2,9 @@
 
 # we need to end the script with a single process that continues to run forever to keep the
 # container alive.
-tail -f /dev/null
+while true; 
+do 
+   echo "AWS_BATCH_JOB_NODE_INDEX is set to ${AWS_BATCH_JOB_NODE_INDEX}"
+   echo "This entrypointscript will sleep for 5 seconds"
+   sleep 5; 
+done
