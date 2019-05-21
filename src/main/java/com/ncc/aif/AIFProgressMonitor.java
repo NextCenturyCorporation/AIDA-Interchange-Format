15f5e6758e1a241c34c4835ee2afd2482bbc67c4
nextcen-dgemoets <darren.gemoets@nextcentury.com>
2019-05-21 15:18:23 -0400

    Remove multi-threadedness.  That will be included in a future progress monitor bundled with the multi-threaded validator.

--- src/main/java/com/ncc/aif/AIFProgressMonitor.java
+++ src/main/java/com/ncc/aif/AIFProgressMonitor.java
@@ -1,62 +1,40 @@
 package com.ncc.aif;
 
 import ch.qos.logback.classic.Logger;
-import com.google.common.collect.Maps;
 import org.topbraid.jenax.progress.NullProgressMonitor;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Thread-safe progress monitor for AIF validation.  Creating multiple instances with the same name will write to the
- * same file in a thread-safe manner.
+ * Progress monitor for AIF validation.
  * Note that this class is implemented based on the way the TopBraid validator engine sends progress monitor updates.
  * Writes a tab-delimited file in the following format:
- * <code>Thread | Shape# | Shape Name | Start Time | End Time | Duration (ms)</code>
- * <code>main | 1 | sh:MinExclusiveConstraintComponent | t1 | t2 | 25ms<code>
- * <code>Thread-2 | 2 | sh:NotConstraintComponent | t1 | t2 | 1225ms<code>
+ * <code>Shape# | Shape Name | Start Time | End Time | Duration (ms)</code>
+ * <code>1 | sh:MinExclusiveConstraintComponent | t1 | t2 | 25ms<code>
+ * <code>2 | sh:NotConstraintComponent | t1 | t2 | 1225ms<code>
  */
 class AIFProgressMonitor extends NullProgressMonitor {
-    private static final boolean LOGGING = true;
+    private static final boolean LOGGING = false;
     private static final Logger logger = (Logger) (org.slf4j.LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME));
     private static final SimpleDateFormat FORMAT = new SimpleDateFormat("HH:mm:ss.SSS");
     private static final int LOGGING_THRESHOLD = 1000;
-    private static final Map<String, BufferedWriter> fileMap = Maps.newConcurrentMap();
     private BufferedWriter out;
-    private Map<Integer, String> shapeNameMap;
-    private Map<Integer, Long> shapeTimeMap;
+    private Map<Integer, String> shapeNameMap = new HashMap<>();
+    private Map<Integer, Long> shapeTimeMap = new HashMap<>();
     private int shapeNum;
     private int numShapes;
 
     AIFProgressMonitor(String name) throws IOException {
-        shapeNameMap = Maps.newConcurrentMap();
-        shapeTimeMap = Maps.newConcurrentMap();
-
-        if (fileMap.containsKey(name)) {
-            out = fileMap.get(name); // reuse the file
-        } else { // create the file and store the mapping
-            out = createOutfile(name);
-            fileMap.put(name, out);
-        }
-    }
-
-    // Create a BufferedWriter in which writes are performed synchronously
-    private BufferedWriter createOutfile(String name) throws IOException {
-        // Create new file with basename based on name
         final String filename = name.replace(".ttl", "-progress.tab");
         log("Creating file " + filename + " for logging validation progress.");
-        final Path outputPath = Paths.get(filename);
-        Files.deleteIfExists(outputPath);
-        Files.createFile(outputPath);
-        // Supplying the DSYNC option requires that the file already exists.
-        return Files.newBufferedWriter(outputPath, StandardOpenOption.DSYNC);
+        out = Files.newBufferedWriter(Paths.get(filename));
     }
 
     private static void log(String text) {
@@ -73,7 +51,7 @@ class AIFProgressMonitor extends NullProgressMonitor {
         log("Logging to file number of shapes: " + this.numShapes);
         try {
             out.write("Total: " + this.numShapes + "\n");
-            out.write("Thread\tShape#\tShape Name\tStart Time\tEnd Time\tDuration (ms)\n");
+            out.write("Shape#\tShape Name\tStart Time\tEnd Time\tDuration (ms)\n");
             out.flush();
         } catch (IOException ioe) {
             System.err.println("Could not write to progress monitor.");
@@ -113,13 +91,11 @@ class AIFProgressMonitor extends NullProgressMonitor {
         }
         // Write a row of the file.
         try {
-            final String row =
-                    Thread.currentThread().getName() + "\t" +
-                            shapeNum + "\t" +
-                            shapeName + "\t" +
-                            FORMAT.format(new Date(startTimeMs)) + "\t" +
-                            FORMAT.format(endTime) + "\t" +
-                            duration + "\n";
+            final String row = shapeNum + "\t" +
+                    shapeName + "\t" +
+                    FORMAT.format(new Date(startTimeMs)) + "\t" +
+                    FORMAT.format(endTime) + "\t" +
+                    duration + "\n";
             out.write(row);
             out.flush();
             if (shapeNum >= numShapes) {
