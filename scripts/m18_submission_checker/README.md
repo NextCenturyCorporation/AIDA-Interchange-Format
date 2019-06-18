baef01da22e877c1cc0e020577dc33e1f480a4d3
Noah Jaffe <njaffe@justins-mbp.hq.nextcentury.com>
2019-06-18 17:20:09 -0400

    changes based on code review - see comments for full list

---
+++ scripts/m18_submission_checker/README.md
@@ -0,0 +1,29 @@
+# See section 9 of the NIST AIDA 2019 Evaluation Plan V1.3 document for proper archive naming convention, formatting, and archive file types. #
+
+This script is designed to help check if an archive is submit ready for M18 evaluation. 
+Submit ready means that the archive is structured properly for the AIF M18 batch validation process.
+
+This script checks that the ttl files are located in a valid directory structure (i.e. not in the base directory when expected to be in a /NIST/ directory).
+
+This script DOES NOT check for valid number of ttl files (i.e. will not find anything wrong with a TA 2 archive with more than 1 ttl in a /NIST/ directory).
+
+This script DOES NOT check that files in the /NIST/ directory are RESTRICTED AIF and that the files in the /INTER-TA/ directory are standard AIF, and so on. 
+
+### To run: ###
+
+    python3 aida_m18_submission_checker.py <path/to/archive>
+    or
+    python aida_m18_submission_checker.py <path/to/archive>
+    
+    
+Example usage and output for a valid TA_1a archive:
+    
+    $ python3 aida_m18_submission_checker.py /Users/njaffe/Documents/archives/OPERA_1.zip 
+    INFO:  Checking archive... this may take a moment for large files.
+    INFO:  archive /Users/njaffe/Documents/archives/OPERA_1.zip is detected to be of task type 1a.
+    INFO:  TA_1a Archive Validator report:
+    INFO:  TTL files found: 1653
+    INFO:  TTL files in valid locations: 1653
+    INFO:  Submission: /Users/njaffe/Documents/archives/OPERA_1.zip  is VALID and submit ready.
+    INFO:  Task Type: 1a | Task Type: 1a | Task Type: 1a | Task Type: 1a | Task Type: 1a | Task Type: 1a | Task Type: 1a | Task Type: 1a | Task Type: 1a | Task Type: 1a |
+    
--- scripts/m18_submission_checker/aida_m18_submission_checker.py
+++ scripts/m18_submission_checker/aida_m18_submission_checker.py
@@ -7,11 +7,11 @@ python3 aida_m18_submission_checker.py <path/to/archive>
 
 Output will be printed to stdout and/or stderr
 '''
-import sys                  #for sys.args
-from sys import exit as exit
-import os                   #for extention access (basename)
-import tarfile, zipfile     #for archive access
-import re                   #for regex
+import sys
+import os
+import tarfile, zipfile
+import re
+import logging
 
 
 '''
@@ -27,21 +27,11 @@ FILETYPE = -1
 #task types to validate for
 TASK_TYPES = ["1a", "1b", "2", "3"]
 
+#the base name of the archive file
+ARCHIVE_FILE_NAME = ''
+
 #list of names of items in the archive - to be populated after determining what filetype the archive is
 ARCHIVE_NAMES = []
-
-
-def eprint(*args, **kwargs):
-    """simple function to print to stderr
-
-    :param args: data to print
-    :param kwargs: keywords to pass in print args
-    """
-    #*****************
-    # REQUIRES PYTHON 3+
-    #*****************
-    print(*args, file=sys.stderr, **kwargs)
-
 
 def do_TA1a_check():
     """
@@ -71,27 +61,27 @@ def do_TA1a_check():
 
 #TODO CHECK TA1A AND TA1B FOR IF PATH_ITEMS[-1] IS EVER NOT THE LOCATION OF THE .TTL FILE NAME
 
-        if '.ttl' in path_items[-1]:
+        if path_items[-1].endswith('.ttl'):
 
-            ttls_found_count = ttls_found_count + 1
+            ttls_found_count += 1
 
             #validate that the ttl file is in an acceptable directory
-            #rule: ttl needs to be in 2nd level -- aka 3rd item in list['name', 'nist', 'valid.ttl']
-            #rule: ttl needs to be in directory named nist, or inter-ta
+            #rule: ttl needs to be in 2nd level -- aka 3rd item in list['name', 'NIST', 'valid.ttl']
+            #rule: ttl needs to be in directory named NIST, or INTER-TA
             try:
-                if ((path_items[-2].upper() == 'NIST' or path_items[-2].upper() == 'INTER-TA') and len(path_items) == 3):
+                if ((path_items[-2] == 'NIST' or path_items[-2] == 'INTER-TA') and len(path_items) == 3):
                     ttls_valid_count = ttls_valid_count + 1
                 else:
-                    ttls_error_warning_count = ttls_error_warning_count + 1
+                    ttls_error_warning_count += 1
             except:
-                ttls_error_warning_count = ttls_error_warning_count + 1
+                ttls_error_warning_count += 1
 
-    print("\nTA_1a Archive Validator report:")
-    print("INFO: TTL files found: " + str(ttls_found_count))
-    print("INFO: TTL files in valid locations: " + str(ttls_valid_count))
+    logging.info("\033[1mTA_1a\033[0m Archive Validator report:")
+    logging.info("TTL files found: " + str(ttls_found_count))
+    logging.info("TTL files in valid locations: " + str(ttls_valid_count))
     if ttls_error_warning_count > 0:
-        print("ERROR: TTL files found in an invalid location: " + str(ttls_error_warning_count))
-        print(".ttl files must be located in either the <run_ID>/NIST/ or <run_id>/INTER-TA directory")
+        logging.error("TTL files found in an invalid location: " + str(ttls_error_warning_count))
+        logging.error(".ttl files must be located in either the <run_ID>/NIST/ or <run_id>/INTER-TA directory")
 
     if ttls_found_count > 0 and ttls_error_warning_count == 0:
         return True
@@ -121,27 +111,27 @@ def do_TA1b_check():
         #split into levels
         path_items = re.split('/', name)
 
-        if '.ttl' in path_items[-1]:
+        if path_items[-1].endswith('.ttl'):
 
-            ttls_found_count = ttls_found_count + 1
+            ttls_found_count += 1
 
             #validate that the ttl file is in an acceptable directory
-            #rule: ttl needs to be in 3rd level -- aka 4th item in list['name', 'nist', 'hypothesis', 'valid.ttl']
-            #rule: ttl needs to be in a subdirectory within the nist directory
+            #rule: ttl needs to be in 3rd level -- aka 4th item in list['name', 'NIST', 'hypothesis', 'valid.ttl']
+            #rule: ttl needs to be in a subdirectory within the NIST directory
             try:
-                if path_items[-3].upper() == 'NIST' or len(path_items) == 4:
+                if path_items[-3] == 'NIST' and len(path_items) == 4:
                     ttls_valid_count = ttls_valid_count + 1
                 else:
-                   ttls_error_warning_count = ttls_error_warning_count + 1
+                   ttls_error_warning_count += 1
             except:
-                ttls_error_warning_count = ttls_error_warning_count + 1
+                ttls_error_warning_count += 1
 
-    print("\nTA_1b Archive Validator report:")
-    print("INFO: TTL files found: " + str(ttls_found_count))
-    print("INFO: TTL files in valid locations: " + str(ttls_valid_count))
+    logging.info("\033[1mTA_1b\033[0m Archive Validator report:")
+    logging.info("TTL files found: " + str(ttls_found_count))
+    logging.info("TTL files in valid locations: " + str(ttls_valid_count))
     if ttls_error_warning_count > 0:
-        print("ERROR: TTL files found in an invalid location: " + str(ttls_error_warning_count))
-        print("ttl files must be located the appropriate hypothesis_id subdirectory of the <run_ID>/NIST/ directory. i.e. <run_id>/NIST/<hypothesis_id>/file.ttl")
+        logging.error("TTL files found in an invalid location: " + str(ttls_error_warning_count))
+        logging.error("ttl files must be located the appropriate hypothesis_id subdirectory of the <run_ID>/NIST/ directory. i.e. <run_id>/NIST/<hypothesis_id>/file.ttl")
 
     if ttls_found_count > 0 and ttls_error_warning_count == 0:
         return True
@@ -175,28 +165,28 @@ def do_TA2_check():
         #split into levels
         path_items = re.split('/', name)
 
-        if '.ttl' in path_items[-1]:
+        if path_items[-1].endswith('.ttl'):
 
-            ttls_found_count = ttls_found_count + 1
+            ttls_found_count += 1
 
             #validate that the ttl file is in an acceptable directory
-            #rule: ttl needs to be in 2nd level -- aka 3rd item in list['name', 'nist', 'valid.ttl']
-            #rule: ttl needs to be in directory named nist, or inter-ta
+            #rule: ttl needs to be in 2nd level -- aka 3rd item in list['name', 'NIST', 'valid.ttl']
+            #rule: ttl needs to be in directory named NIST, or INTER-TA
             try:
-                if ((path_items[-2].upper() == 'NIST' or path_items[-2].upper() == 'INTER-TA') and len(path_items) == 3):
+                if ((path_items[-2] == 'NIST' or path_items[-2] == 'INTER-TA') and len(path_items) == 3):
                     ttls_valid_count = ttls_valid_count + 1
                 else:
-                    ttls_error_warning_count = ttls_error_warning_count + 1
+                    ttls_error_warning_count += 1
             except:
-                ttls_error_warning_count = ttls_error_warning_count + 1
+                ttls_error_warning_count += 1
 
 
-    print("\nTA_2 Archive Validator report:")
-    print("INFO: TTL files found: " + str(ttls_found_count))
-    print("INFO: TTL files in valid locations: " + str(ttls_valid_count))
+    logging.info("\033[1mTA_2\033[0m Archive Validator report:")
+    logging.info("TTL files found: " + str(ttls_found_count))
+    logging.info("TTL files in valid locations: " + str(ttls_valid_count))
     if ttls_error_warning_count > 0:
-        print("ERROR: TTL files found in an invalid location: " + str(ttls_error_warning_count))
-        print(".ttl files must be located in either the <run_ID>/NIST/ or <run_id>/INTER-TA directory")
+        logging.error("TTL files found in an invalid location: " + str(ttls_error_warning_count))
+        logging.error(".ttl files must be located in either the <run_ID>/NIST/ or <run_id>/INTER-TA directory")
 
     if ttls_found_count > 0 and ttls_error_warning_count == 0:
         return True
@@ -219,50 +209,50 @@ def do_TA3_check():
 
     ttls_found_count = 0
     ttls_error_warning_count = 0
+    ttls_valid_count = 0
 
     for name in ARCHIVE_NAMES:
-        print(name)
+
         #split into levels
         path_items = re.split('/', name)
-        print(path_items)
-        if '.ttl' in path_items[-1]:
 
-            ttls_found_count = ttls_found_count + 1
+        if path_items[-1].endswith('.ttl'):
+
+            ttls_found_count += 1
 
             #validate that the ttl file is in an acceptable directory
-            #rule: ttl needs to be in 2nd level -- aka 3rd item in list['name', 'nist', 'valid.ttl']
-            #rule: ttl needs to be in directory named nist, or inter-ta
+            #rule: ttl needs to be in 2nd level -- aka 3rd item in list['name', 'NIST', 'valid.ttl']
+            #rule: ttl needs to be in directory named NIST, or INTER-TA
 
-            if len(path_items) is not 2:
-                ttls_error_warning_count = ttls_error_warning_count + 1
+            if len(path_items) == 2:
+                ttls_valid_count += 1
+            else:
+                ttls_error_warning_count += 1
 
-    print("\nTA_3 Archive Validator report:")
-    print("INFO: TTL files found: " + str(ttls_found_count))
-    print("INFO: TTL files in valid locations: " + str(ttls_found_count - ttls_error_warning_count))
+    logging.info("\033[1mTA_3\033[0m Archive Validator report:")
+    logging.info("TTL files found: " + str(ttls_found_count))
+    logging.info("TTL files in valid locations: " + str(ttls_valid_count))
     if ttls_error_warning_count > 0:
-        print("ERROR: TTL files found in an invalid location: " + str(ttls_error_warning_count))
-        print(".ttl files must be located in either the <run_ID>/NIST/ or <run_id>/INTER-TA directory")
+        logging.error("TTL files found in an invalid location: " + str(ttls_error_warning_count))
+        logging.error(".ttl files must be located in either the <run_ID>/NIST/ or <run_id>/INTER-TA directory")
 
     if ttls_found_count > 0 and ttls_error_warning_count == 0:
         return True
     return False
 
 
-def get_task_type(file, args):
+def get_task_type():
     """
-
-    :param file: the archive file
-    :param args: the runtime arguments for the program -- used to determine the task type
-    :return: int corresponding to the task type (see TASK_TYPE)
+    :return: int corresponding to the task type depending on the archive file name (see TASK_TYPE)
     :rtype: int
     """
 
     #user did not tell us what task type it was, we must determine it ourselves.
-    #do this with counting the number of .'s in the filename (get rid of .tar.gz first though)
-    basename = os.path.basename(file)
+    #count the number of .'s in the archive file name to determine the task type. Hope the user has the proper naming convention.
+    basename = os.path.basename(ARCHIVE_FILE_NAME)
 
-    #get rid of extra count if extention is .tar.gz
-    dot_count = basename.count('.') if not '.tar.gz' in basename else basename.count('.') - 1
+    #get rid of extra . count if the file extention is .tar.gz
+    dot_count = basename.count('.') if not basename.endswith('.tar.gz') else basename.count('.') - 1
 
     if dot_count == 1:
         #if theres 1 dot_count, we need to check to see if it is 1a or 1b.
@@ -281,42 +271,41 @@ def get_task_type(file, args):
         ta1a_dir_struct_regex = re.compile(".*\/NIST\/[^\/]+.ttl", re.IGNORECASE)
         ta1b_dir_struct_regex = re.compile(".*\/NIST\/\S*\/\S*\.ttl", re.IGNORECASE)
 
-        #if ttls located in the nist directory, mark as potential TA_1a
+        #if ttls located in the NIST directory, mark as potential TA_1a
         if ta1a_dir_struct_regex.search(names_str) is not None:
             ta1_ret_val = 0
             type_count = type_count + 1
-        #if ttls located at some subdirectory of the nist directory, mark as potential TA_1b
+        #if ttls located at some subdirectory of the NIST directory, mark as potential TA_1b
         if ta1b_dir_struct_regex.search(names_str) is not None:
             ta1_ret_val = 1
             type_count = type_count + 1
-        #if ttls were found in both the nist/ directory and a subdirectory of nist/, mark as TA_1a and have the ta_1a do this check too
+        #if ttls were found in both the NIST/ directory and a subdirectory of NIST/, mark as TA_1a and have the ta_1a do this check too
         if ta1_ret_val > -1 and type_count > 1 :        #use > and not == for scaleability? idk dont @ me c:
-            print ("WARNING: Subdirectories found at the same level of .ttl files in the NIST directory.")
+            logging.warning("WARNING: Subdirectories found at the same level of .ttl files in the NIST directory.")
             ta1_ret_val = 0
         return ta1_ret_val
     elif dot_count > 1 and dot_count < 4:
         return dot_count
     else:
-        eprint("Unkown archive filetype. Please ensure archive name is properly titled per NIST guidelines.")
+        logging.error("Cannot figure out the TA level. Please ensure archive name is properly titled per NIST guidelines.")
         return -1
 
 
 
-def validate_archive_filetype(file):
+def validate_archive_filetype():
     """
-    Checks the input file type. If it is not a valid filetype, the program will
+    Checks the archive file type. If it is not a valid filetype, the program will
     print an error message and quit the script.
     Valid filetypes: .tgz, .tar.gz, or .zip.
 
-    :param file: path to file including file name and extention
     :return: True if valid filetype and successfully set the global FILETYPE variable, False otherwise
     :rtype: bool
     """
     global FILETYPE
-    file_ext = os.path.splitext(file)[-1].lower()
-    if file_ext == ".gz" or file_ext == ".tgz":
+
+    if ARCHIVE_FILE_NAME.endswith('.tar.gz') or ARCHIVE_FILE_NAME.endswith('.tgz'):
         FILETYPE = 0
-    elif file_ext == ".zip":
+    elif ARCHIVE_FILE_NAME.endswith('.zip'):
         FILETYPE = 1
     else:
         FILETYPE = -1
@@ -333,23 +322,43 @@ def main():
     global FILETYPE
     global TASK_TYPES
     global ARCHIVE_NAMES
+    global ARCHIVE_FILE_NAME
+
+    # set logging to log to stdout
+    logging.basicConfig(level='INFO', format='%(levelname)s:  %(message)s')
+
+    #check runtime arguments
+    if len(sys.argv) < 2:
+        logging.error("Missing archive file as argument.\nTo run: python3 aida_m18_submission_checker.py <path/to/archive>")
+        return False
+    if not os.path.isfile(sys.argv[1]):
+        logging.error("Argument is not a file.\nTo run: python3 aida_m18_submission_checker.py <path/to/archive>")
+        return False
+
+    ARCHIVE_FILE_NAME = str(sys.argv[1])
 
     #ensure the archive type is a valid filetype and set the global FILETYPE value
-    if not validate_archive_filetype(sys.argv[1]) and FILETYPE >= 0:
-        eprint("FATAL ERROR: Archive filetype unknown. Please use .zip, .tgz, or .tar.gz")
-        exit(False)
+    if validate_archive_filetype() < 0:
+        logging.critical("Archive filetype unknown. Please use .zip, .tgz, or .tar.gz")
+        return False
 
     #get names of items in the archive
-    if '-v' in sys.argv: print ("Gathering archive items...")
-    archive = zipfile.ZipFile(sys.argv[1], 'r', allowZip64=True) if FILETYPE else tarfile.open(sys.argv[1], mode='r')
-    ARCHIVE_NAMES = archive.namelist() if FILETYPE else archive.getnames()
+    try:
+        logging.info("Checking archive... this may take a moment for large files.")
+        archive = zipfile.ZipFile(ARCHIVE_FILE_NAME, 'r', allowZip64=True) if FILETYPE else tarfile.open(ARCHIVE_FILE_NAME, mode='r')
+        ARCHIVE_NAMES = archive.namelist() if FILETYPE == 1 else archive.getnames()
+    except:
+        #expected errors: (zipfile.BadZipFile, tarfile.ReadError), but catch all of them anyways
+        logging.critical("Error thrown attempting to read/open the archive. Please use 'zip' or 'tar' to create your archive.")
+        return False
 
-    task_type = get_task_type(sys.argv[1], sys.argv)
+
+    task_type = get_task_type()
     if task_type == -1:
-        eprint ("ERROR: Cannot determine the task type! Please input task type to arguments and run again.")
-        exit(False)
+        logging.error("Cannot determine the task type! Please input task type to arguments and run again.")
+        return False
 
-    print("INFO: archive " + str(sys.argv[1]) + " is detected to be of Task type " + TASK_TYPES[task_type] + ".")
+    logging.info("archive " + str(ARCHIVE_FILE_NAME) + " is detected to be of task type \033[1m" + TASK_TYPES[task_type] + "\033[0m.")
 
     check_status = False
     if task_type == 0:
@@ -363,8 +372,12 @@ def main():
 
 
     is_or_not = " is " if check_status else " is not "
-    print(sys.argv[1] + is_or_not + "submit ready.")
+    result_str = "\033[1mVALID\033[0m and submit ready." if check_status else "\033[1mINVALID\033[0m."
+    logging.info("Submission: " + ARCHIVE_FILE_NAME + " is " + result_str)
 
+    logging.info("Task Type: \033[1m{0}\033[0m | ".format(TASK_TYPES[task_type]) * 10)
+
+    return check_status
 
 
 if __name__ == "__main__": main()
